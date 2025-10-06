1225d047fe88ccdf3b1c79a68466ebcd27b1abca
lilas <lilaskaakour@gmail.com>
2025-10-06 12:02:49 +0100

    test

--- 01.drawing/sketch.js
+++ 01.drawing/sketch.js
@@ -4,5 +4,7 @@ function setup() {
 
 function draw() {
   background(220);
-  circle(50, 50, 50)
-}
+  fill(onabort,200,0)
+  stroke(100,20,200)
+  circle(mouseX, 50, 50)
+rect(100,100,200,200,10,10,50,5)}
