d24fbc3bc704ae12268ad7ad824eaeb27a304ab3
lilas <lilaskaakour@gmail.com>
2025-10-13 11:40:28 +0100

    change

--- 01.drawing/sketch.js
+++ 01.drawing/sketch.js
@@ -1,10 +1,25 @@
+let house = 33;
+var mouse = 66;
+let name = "lilas";
+let hot = true;
+let idontknow;
+let temp = 55.88;
+let myth
+
 function setup() {
+let mywith = 640;
   createCanvas(400, 400);
+  let person = 55;
 }
 
 function draw() {
   background(220);
+  //cirlce(50,50 50)
   fill(onabort,200,0)
   stroke(100,20,200)
   circle(mouseX, 50, 50)
-rect(100,100,200,200,10,10,50,5)}
+rect(100,100,200,200,10,10,50,5)
+console.log(house)
+house = house+1
+}
+
---
+++ 02.movement/sketch.js
@@ -0,0 +1,25 @@
+let house = 33;
+let name = "lilas";
+let temp = true;
+let idontknow;
+let xVal = 100;
+let yVal = 100;
+
+function setup() {
+  let mywidth = 648;
+  createCanvas(400, 400);
+  let person = 55;
+  red = random(20,255)
+  blue = random(0,200)
+}
+
+
+function draw() {
+  let mySize = 50;
+  background(220,0,100)
+  //circle(50,50 50)
+  circle(house, 50, 35);
+  console,log(house)
+  house = house+1
+
+}
